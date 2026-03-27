320f38fda00411104aa7f66af166e79f952697f8
Richard <richardlwhittington@gmail.com>
2026-03-27 01:07:21 -0500

    create go modules and finish tutorial

---
+++ greetings/greetings.go
@@ -0,0 +1,9 @@
+package greetings
+
+import "fmt"
+
+var Points = []int{20, 90, 100, 45, 70}
+
+func SayHola(n string) {
+	fmt.Println("Hello", n)
+}
--- main.go
+++ main.go
@@ -1,8 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+    "gopro/greetings"
+)
+
+func sayGreeting(n string){
+	fmt.Printf("Good morning")
+}
+//_ shows you don't want the index
+func cycleNames(n []string, f func(string)) {
+	for _, value := range n {
+		f(value)
+	}
+}
+//must sprecify type going in and for the output
+func sayHello(name string) string {
+	fmt.Printf("Hello %v", name)
+	return name
+}
+
+func sayHi(name string, age int) (string, int) {
+	fmt.Printf("Hello %v, you are %v years old", name, age)
+	return name, age
+}
+
+	//structs=structures, like maps but can store multiple data types
+	type Book struct {
+	ID     uint
+	Title  string
+	Author string
+	Year   int
+	UserID uint
+}
 
 func main() {
+	//working with scructs
+		var book1 Book
+
+	book1 = Book{1, "Jane Eyre", "Charlotte Bronte", 1990, 6}
+
+	fmt.Println(book1)
+
+//to change individual values of structs
+	book1.Title = "Things Fall Apart"
+	book1.Author = "Chinua Achebe"
+
+	fmt.Println(book1)
+//calling functions
+	sayGreeting("Bob")
+	cycleNames([]string{"emy", "pearl"}, sayGreeting)
+    sayHello("Emy")
+	sayHi("Bob", 32)
+
 	// strings
 	
     var nameOne string = "emy"
@@ -50,21 +100,21 @@ var ages = [3]int{20,25,30}
 fmt.Println(ages, len(ages))
 
 //slices- like arrays but you don't have to specify size
-var scores = []int{100,50,60}
-//updates item at index 2
-scores[2] = 25
-scores = append(scores, 50)
-fmt.Println(scores, len(scores))
+// var scores = []int{100,50,60}
+// //updates item at index 2
+// scores[2] = 25
+// scores = append(scores, 50)
+// fmt.Println(scores, len(scores))
 
-//start at index 0 and continue to index 2
-rangeOne := scores[0:3]
-//slice index 2 through end
-rangeTwo := scores[2:]
-//slice from beginning up to but not including index 3
-rangeThree := scores[:3]
-fmt.Println(rangeOne)
-fmt.Println(rangeTwo)
-fmt.Println(rangeThree)
+// //start at index 0 and continue to index 2
+// rangeOne := scores[0:3]
+// //slice index 2 through end
+// rangeTwo := scores[2:]
+// //slice from beginning up to but not including index 3
+// rangeThree := scores[:3]
+// fmt.Println(rangeOne)
+// fmt.Println(rangeTwo)
+// fmt.Println(rangeThree)
 
 //loops
 x := 0
@@ -92,5 +142,36 @@ for index, value := range elves {
 	//if you only want to use one use _
 	for _, value := range elves {
 		fmt.Printf("the value is %v \\\\n", value)
-}
+}//maps-declare types of keys and values- can only store one type of key and one of value
+scores := map[string]float64{
+		"maths":           20,
+		"english":         15,
+		"french":          14,
+		"spanish":         12,
+	}
+
+	fmt.Println(scores)
+	fmt.Println(scores["maths"])
+
+	//loops maps
+	for key, value := range scores {
+		fmt.Println(key, "-", value)
+	}
+
+	//it's important to note that maps are reference types. Reference types are types whose variables don’t store the actual data, but rather an internal pointer to the actual data. This means that if the same data is assigned to multiple variables, when one instance gets modified, the original gets modified as well.
+
+	scores2 := scores
+
+	scores2["maths"] = 15
+	fmt.Println(scores)
+
+	sayHello("Bob")
+
+
+
+		greetings.SayHola("emy")
+
+	for _, v := range greetings.Points {
+		fmt.Println(v)
+	}
 }
